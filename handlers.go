d834f24c354abeca5a85b6de6bbec8f48b721483
moisesrendev-coder <moisesrendev@gmail.com>
2026-09-13 00:28:51 -0700

    feat: Add login POST handler with form validation

--- handlers.go
+++ handlers.go
@@ -4,38 +4,59 @@ import (
 	"net/http"
 )
 
-var htmlContent = `
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>%s</title>
-</head>
-<body>
-	<h1>About this simple server</h1>
-	<p>This server is built using Go.</p>
-	<p>%s</p>
-</body>
-</html>
-`
-
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Printf("%s %s", r.Method, r.URL.Path)
-	app.render(w, "index.html", nil)
+	//app.infoLog.Printf("Session Data: %s", app.session.GetString(r, "userID"))
+	app.render(w, r, "index.html", nil)
 }
 
 func (app *application) login(w http.ResponseWriter, r *http.Request) {
-	app.render(w, "login.html", nil)
+	//app.session.Put(r, "userID", "moises")
+
+	if r.Method == http.MethodPost {
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		form := NewForm(r.PostForm)
+		form.Required("email", "password").
+			MaxLength("email", 255).
+			MaxLength("password", 255).
+			MinLength("password", 6).
+			MinLength("email", 3).
+			Matches("email", EmailRX)
+
+		if !form.Valid() {
+			form.Errors.Add("generic", "The data you submitted was not valid!")
+			app.render(w, r, "login.html", &templateData{
+				Form: form,
+			})
+			return
+		}
+
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+
+		app.infoLog.Printf("Logged in with email %s; %s\n", email, password)
+	}
+
+	app.render(w, r, "login.html", &templateData{
+		Form: NewForm(r.PostForm),
+	})
 }
 
 func (app *application) register(w http.ResponseWriter, r *http.Request) {
-	app.render(w, "register.html", nil)
+	app.render(w, r, "register.html", nil)
 }
 
 func (app *application) about(w http.ResponseWriter, r *http.Request) {
-	app.render(w, "about.html", nil)
+	app.render(w, r, "about.html", nil)
 }
 
 func (app *application) contact(w http.ResponseWriter, r *http.Request) {
-	app.render(w, "contact.html", nil)
+	app.render(w, r, "contact.html", nil)
+}
+
+func (app *application) submit(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "submit.html", nil)
 }
